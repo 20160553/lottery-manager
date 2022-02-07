54a7e9f349ebdb8578dc25b8f331a0626d236da8
20160553 <97psh@kumoh.ac.kr>
2022-02-07 12:48:50 +0900

    First Commit

---
+++ app/src/main/java/aqper/side_project/lotterymanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package aqper.side_project.lotterymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
